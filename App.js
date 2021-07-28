9dd95b9202254c283fea0042ed614bc77b7a0028
Amrit <amrittheengineer@users.noreply.github.com>
2021-07-29 00:49:26 +0530

    App.js refactored.

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
 import ScannerScreen from "./screens/ScannerScreen";
 import PharmacyScannerScreen from "./screens/PharmacyScannerScreen";
 import ProfileScreen from "./screens/ProfileScreen";
